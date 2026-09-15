8f6eccd7c64d72c838e794890bceb33de421f8ab
bordhanoishe-spec <bordhanoishe@gmail.com>
2026-09-15 23:35:10 +0600

    Add Hexadecimal to Octal conversion program

---
+++ OOP JAVA BASIC PROGRAMMING LAB 3 /Hexadecimaltooctal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HexadecimalToOctal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a hexadecimal number: ");
+        String hex = sc.next();
+
+        int decimal = Integer.parseInt(hex, 16);
+
+        System.out.println("Equivalent octal number: "
+                + Integer.toOctalString(decimal));
+    }
+}
