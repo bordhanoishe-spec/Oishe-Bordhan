cfae3f8cccd317adf8e9b431a52ed99794ac962d
bordhanoishe-spec <bordhanoishe@gmail.com>
2026-09-15 23:46:47 +0600

    Add Dog class with name and color attributes

---
+++ OOP JAVA BASIC PROGRAMMING LAB 6/Dog.java
@@ -0,0 +1,17 @@
+public class Dog {
+
+    String name;
+    String color;
+
+    Dog(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog("Tommy", "Brown");
+
+        System.out.println("Name: " + d.name);
+        System.out.println("Color: " + d.color);
+    }
+}
