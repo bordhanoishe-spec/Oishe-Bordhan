4b3d47e13bb9a6f90826785a22db5543d72020fb
bordhanoishe-spec <bordhanoishe@gmail.com>
2026-09-15 23:44:54 +0600

    Add AverageNumber class to calculate average

---
+++ OOP JAVA BASIC PROGRAMMING LAB 6/Averagenumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AverageNumber {
+
+    static double findAverage(double a, double b, double c) {
+        return (a + b + c) / 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input the first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Input the second number: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Input the third number: ");
+        double c = sc.nextDouble();
+
+        System.out.println("The average value is "
+                + findAverage(a, b, c));
+    }
+}
